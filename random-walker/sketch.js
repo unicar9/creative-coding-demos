156489cc652c2fb414f3ddb7ba409f5739b3ea06
Unicar <unicar9@gmail.com>
2018-09-21 19:28:31 +1000

    Added saturation and brightness controls

--- random-walker/sketch.js
+++ random-walker/sketch.js
@@ -1,65 +1,71 @@
+/*
+Todo:
+1. mono color: adjust hue and shades
+2. colorful mode: H random, adjust only S and B (done)
+3. pause and download(in another panel)
+4. shapes: rect, circle (through radius) and emojis
+*/
+
 
 const Controls = function() {
     this.minSize = 5
     this.maxSize = 20
-    this.step = 5
-};
+    this.step = 15
+    this.radius = 0
+    this.saturation = 48
+    this.brightness = 80
+}
 
 const controls = new Controls()
 
 window.onload = function() {
     const gui = new dat.GUI()
-    gui.add(controls, 'minSize', 1, 10)
-    gui.add(controls, 'maxSize', 20, 50)
+    gui.add(controls, 'minSize', 1, 20)
+    gui.add(controls, 'maxSize', 30, 70)
     gui.add(controls, 'step', 1, 50)
+    gui.add(controls, 'radius', 0, 100)
+    gui.add(controls, 'saturation', 0, 100)
+    gui.add(controls, 'brightness', 0, 100)
 }
 
-// console.log('haha')
-let x, y, hue
-// let bricks = []
-// let size
+let x, y
 
 function setup() {
-  createCanvas(windowWidth, windowHeight)
-  x = windowWidth / 2
-  y = windowHeight / 2
-  rectMode('CENTER')
-  colorMode(HSB)
-  background(0)
+    createCanvas(windowWidth, windowHeight)
+    x = windowWidth / 2
+    y = windowHeight / 2
+    rectMode('CENTER')
+    colorMode(HSB)
+    background(0)
 }
 
-
 function draw() {
-
-  size = random(controls.minSize, controls.maxSize)
-  let roundCorner = 3
-  let hue = floor(random(360))
-  noStroke()
-  fill(hue, 40, 80)
-  rect(x, y, size, size, roundCorner)
+    size = random(controls.minSize, controls.maxSize)
+    let roundCorner = controls.radius
+    let hue = floor(random(360))
+    noStroke()
+    fill(hue, controls.saturation, controls.brightness)
+    rect(x, y, size, size, roundCorner)
   
-  let r = floor(random(4))
+    let r = floor(random(4))
   
-  switch(r) {
-    case 0: 
-      x = (x + controls.step) % windowWidth 
-      break
-    case 1:
-      x = Math.abs( (x - controls.step) % windowWidth )
-      break
-    case 2:
-      y = (y + controls.step) % windowHeight
-      break
-    case 3: 
-      y = Math.abs( (y - controls.step) % windowHeight )
-      break
-  }
-
-//   rect(x, y, size, size, roundCorner)
-
+    switch(r) {
+        case 0: 
+        x = (x + controls.step) % windowWidth 
+        break
+        case 1:
+        x = Math.abs( (x - controls.step) % windowWidth )
+        break
+        case 2:
+        y = (y + controls.step) % windowHeight
+        break
+        case 3: 
+        y = Math.abs( (y - controls.step) % windowHeight )
+        break
+    }
   
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight)
+    resizeCanvas(windowWidth, windowHeight)
 }
