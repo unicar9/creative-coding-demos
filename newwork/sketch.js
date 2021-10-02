0547acc1aecdcc89ba10a7c6d18fdbd50e932dc4
Lingxiao Wang <xwangl213@gmail.com>
2021-10-02 16:21:43 +1000

    updated

--- newwork/sketch.js
+++ newwork/sketch.js
@@ -7,6 +7,8 @@ let values;
 
 let dayIndex = 0;
 
+const infoPanel = document.getElementById("infoPanel");
+
 const colorSchemes = [
   ["#E883EB", "#F044E7", "#BD4CC7", "#9B39B3", "#8D00A3", "#AC92EB"],
   ["#a3daff", "#1ec0ff", "#0080ff", "#03a6ff", "#003399", "#4EC2E8"],
@@ -22,11 +24,11 @@ const colorSchemes = [
 // ["#7e152e", "#d51600", "#e74d08", "#f42448", "#f96574", "#ED5564"],
 
 function groupDataByDate(data) {
-  const min = _.minBy(data, (item) => item[1]);
+  const min = _.minBy(data, item => item[1]);
   console.log("min: ", min);
-  const max = _.maxBy(data, (item) => item[1]);
+  const max = _.maxBy(data, item => item[1]);
   console.log("max: ", max);
-  return _.groupBy(data, (item) => item[0].split(" ")[0]);
+  return _.groupBy(data, item => item[0].split(" ")[0]);
 }
 
 // let monoSynth;
@@ -59,10 +61,9 @@ function draw() {
   textSize(32);
   text(x, 100, 100);
 
-  if (particles.length < values[dayIndex].length) {
+  if (particles.length < values[dayIndex].length && frameCount % 12 === 0) {
     let v = values[dayIndex][count];
     let h = v[0].split(" ")[1].split(":")[0];
-
     let beforeNoon = h < "12";
     particles.push(new Particle(v[1], beforeNoon));
 
@@ -75,6 +76,9 @@ function draw() {
   for (let i = 0; i < particles.length; i++) {
     let particle = particles[i];
     particle.display();
+    if (dist(mouseX, mouseY, particle.pos.x, particle.pos.y) < 10) {
+      updateInfo(values[dayIndex][i]);
+    }
     // particle.playSound();
   }
   // let note = random(["Fb4", "G4"]);
@@ -102,16 +106,40 @@ function draw() {
 //   monoSynth.play(note, velocity, time, dur);
 // }
 
-function mousePressed() {
+function switchDay(i) {
   if (growing) return;
+  x = days[i];
+  if (!x) return;
 
   clear();
   background(0);
   particles = [];
   count = 0;
-  dayIndex++;
-  x = days[dayIndex];
-  console.log(days[dayIndex]);
 
-  if (!x) return;
+  console.log(days[i]);
+}
+
+function mousePressed() {
+  dayIndex++;
+  switchDay(dayIndex);
+}
+
+function keyPressed() {
+  if (growing) return;
+
+  console.log("pressed");
+  if (keyCode === 39 && dayIndex < 30) {
+    // left or right arrow keys
+    dayIndex++;
+    switchDay(dayIndex);
+  }
+
+  if (keyCode === 37 && dayIndex > 0) {
+    dayIndex--;
+    switchDay(dayIndex);
+  }
+}
+
+function updateInfo(content) {
+  infoPanel.textContent = content;
 }
